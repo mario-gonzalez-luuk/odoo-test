2a1b932cbe166c9f1d4993a2fa54e2e7209be66f
mario-gonzalez-luuk <mario.gonzalez@goluuk.com>
2022-03-08 16:47:27 -0600

    :sparkles: Add res_users.py

---
+++ sale_assigner/models/res_users.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    status = fields.Selection(
+        [('available', 'Available'), ('busy', 'Busy'), ('offline', 'Offline')],
+        required=True, string="Status")
