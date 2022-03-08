d457f853b22ea00c8f3a670ea1406edb28e488bc
mario-gonzalez-luuk <mario.gonzalez@goluuk.com>
2022-03-08 17:09:01 -0600

    :sparkles: Add compute method for sale_order_assigned_qty

--- sale_assigner/models/res_users.py
+++ sale_assigner/models/res_users.py
@@ -10,5 +10,10 @@ class ResUsers(models.Model):
         [('available', 'Available'), ('busy', 'Busy'), ('offline', 'Offline')],
         required=True, string="Status")
     sale_order_ids = fields.One2many(comodel_name="sale.order", inverse_name="employee_id")
-    sale_order_assigned_qty = fields.Integer(string="Sale Order Assigned Quantity")
+    sale_order_assigned_qty = fields.Integer(compute="_compute_sale_order_assigned_qty",
+                                             string="Sale Order Assigned Quantity")
 
+    @api.depends("sale_order_ids")
+    def _compute_sale_order_assigned_qty(self):
+        for record in self:
+            record.sale_order_assigned_qty = len(record.sale_order_ids)
