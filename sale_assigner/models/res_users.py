0d0b167645b06afef2eb65fd7baaa8735c8cd5de
mario-gonzalez-luuk <mario.gonzalez@goluuk.com>
2022-03-08 17:29:37 -0600

    :recycle: Change variable status to state in ResUsers

--- sale_assigner/models/res_users.py
+++ sale_assigner/models/res_users.py
@@ -6,7 +6,7 @@ from odoo import api, fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    status = fields.Selection(
+    state = fields.Selection(
         [('available', 'Available'), ('busy', 'Busy'), ('offline', 'Offline')],
         required=True, string="Status")
     sale_order_ids = fields.One2many(comodel_name="sale.order", inverse_name="employee_id")
