ac52b6bf5696835ee74c544cbead7be0f3cd6471
mario-gonzalez-luuk <mario.gonzalez@goluuk.com>
2022-03-08 17:27:23 -0600

    :construction: Add methods to compute employee_id

--- sale_assigner/models/sale_order.py
+++ sale_assigner/models/sale_order.py
@@ -6,4 +6,14 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    employee_id = fields.Many2one(comodel_name="res.users", string="Employee")
+    employee_id = fields.Many2one(compute="_compute_employee_id", comodel_name="res.users", string="Employee")
+
+    @api.depends("state")
+    def _compute_employee_id(self):
+        for record in self:
+            if record.state == "sale":
+                record.employee_id = _assign_sale_order()
+
+    def _assign_sale_order(self):
+        # TODO
+        pass
