7ec05bc1b0f6362bf164b9f64f8631999c057b7c
mario-gonzalez-luuk <mario.gonzalez@goluuk.com>
2022-03-08 15:23:17 -0600

    :tada: Initial commit

---
+++ sale_assigner/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sales Assigner',
+    'version': '0.1',
+    'category': 'Sales/User',
+    'sequence': 15,
+    'summary': 'Link module for sale and user modules',
+    'description': "",
+    'author': "Luuk",
+    'website': 'https://www.goluuk.com/',
+    'depends': ['base', 'sale'],
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
