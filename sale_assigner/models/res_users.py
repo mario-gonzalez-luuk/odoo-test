d5de247a2e074790310b2ff71fb3e1cabc2f3e5c
mario-gonzalez-luuk <mario.gonzalez@goluuk.com>
2022-03-08 17:05:05 -0600

    :sparkles: Add sale_order_ids to ResUsers

--- sale_assigner/models/res_users.py
+++ sale_assigner/models/res_users.py
@@ -9,4 +9,6 @@ class ResUsers(models.Model):
     status = fields.Selection(
         [('available', 'Available'), ('busy', 'Busy'), ('offline', 'Offline')],
         required=True, string="Status")
+    sale_order_ids = fields.One2many(comodel_name="sale.order", inverse_name="employee_id")
     sale_order_assigned_qty = fields.Integer(string="Sale Order Assigned Quantity")
+
